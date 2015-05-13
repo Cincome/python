6fc04d686d22b057ddc94677634a1f1806d613a6
Cincome <maryelizabeth418@yahoo.com>
2015-05-13 15:17:00 -0500

    Add Exercises

---
+++ ex5.py
@@ -0,0 +1,10 @@
+my_name= 'Zed A. Shaw'
+my_age= 35
+my_height= 74
+my_weight= 180
+
+print "Let's talk about %s." % my_name
+print "Heis %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight , my_age + my_height+my_weight)
